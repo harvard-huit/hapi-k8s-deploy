e1c9b6a7b97de5bc7a3ff77ae1afb64677ca72bc
Mark Stacy <mark_stacy@harvard.edu>
2023-10-20 14:41:21 -0400

    rollout restart convert string

--- src/k8sdeploy/k8sdeploy.py
+++ src/k8sdeploy/k8sdeploy.py
@@ -158,10 +158,10 @@ class KubernetesDeploy():
                 temp_file.write(rendered)
             if template == "deployment":
                 result=run(['kubectl', action ,'-f',temp_file.name ],capture_output=True)
-                print(result.stdout)
-                if "unchanged" in result.stdout:
+                print(result.stdout.decode('utf-8'))
+                if "unchanged" in result.stdout.decode('utf-8'):
                     self.rollout_restart=True
-                print(result.stdout) #,result.stderr)
+                print(result.stderr.decode('utf.8')) 
             else:
                 run(['kubectl', action ,'-f',temp_file.name ])
         except Exception as e:
